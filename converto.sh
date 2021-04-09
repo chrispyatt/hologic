6df2b789553380e4a97c9530cd6c0f16fd5f6e67
chrispyatt <chris-pyatt@live.co.uk>
2021-04-09 12:56:51 +0100

    two scripts is best

--- converto.sh
+++ converto.sh
@@ -8,6 +8,11 @@ fi
 
 infile=$1
 
+echo -----------------------------------
+echo ------- Supplied input file -------
+echo -----------------------------------
+echo $infile
+
 # check dicom compliance
 echo -------------------------------------------------------
 echo ------- Checking DICOM compliance with DCIODVFY -------
@@ -18,17 +23,23 @@ dciodvfy $infile
 echo ------------------------------------------------
 echo ------- Loading provate DICOM dictionary -------
 echo ------------------------------------------------
-export DCMDICTPATH=./hologic_private.dic
+export DCMDICTPATH=./hologic_private.dic:$DCMDICTPATH
 
 echo ---------------------------------------------
 echo ------- Getting private data elements -------
 echo ---------------------------------------------
-dcmdump +L +P "7e01,1012" --prepend +uc $infile | grep "(7e01,1010).(7e01,1012)" | sed 's/^.*OB //;s/ #.*$//' | split -d -l1
+dcmdump +L +P "7e01,1012" --prepend +uc $infile \
+| grep "(7e01,1010).(7e01,1012)" \
+| sed 's/^.*OB //;s/ #.*$//' \
+| split -d -l1
 
-for i in ls x0*; do
+for i in x0*; do
     xxd -r -p $i $i.bin
+    rm $i
 done
 
 cat x*.bin > data.bin
 rm x*.bin
+
+
 
